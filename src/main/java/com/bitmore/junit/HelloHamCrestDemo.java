f71edc8ddf8ec4725b50594b2ea65e4de9691d65
Phanidhar K <phanidhar.kammara@gmail.com>
2014-06-10 20:10:29 -0400

    Changes Added

--- src/main/java/com/bitmore/junit/HelloHamCrestDemo.java
+++ src/main/java/com/bitmore/junit/HelloHamCrestDemo.java
@@ -1,5 +1,8 @@
 package com.bitmore.junit;
 
 public class HelloHamCrestDemo {
-
+	public static void main(String[] args) {
+		System.out.println("HelloHamCrestDemo Main Start...");
+		System.out.println("HelloHamCrestDemo Main End...");
+	}
 }
